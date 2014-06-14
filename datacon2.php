072cb9efea529de9f1d45638f0ba910c94cbfc9a
johntrombly <john@trombly.org>
2014-06-14 16:02:17 -0500

    Update datacon2.php
    Changed connection to conn in one of your comments

--- datacon2.php
+++ datacon2.php
@@ -2,7 +2,7 @@
 // this is a comment line from John
 $dblink = mysqli_connect("localhost","root","","cumm7860_milhist");
 
-// Check connection
+// Check conn
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
