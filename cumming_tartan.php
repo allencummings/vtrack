df9b26e86fd5a98644b540bc2adbff1bc5dd5522
allencummings <allencummings@verizon.net>
2014-06-14 15:55:43 -0500

    Add cumming_tartan.php
    Add new page

---
+++ cumming_tartan.php
@@ -0,0 +1,264 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Windows-1252">
+<title>Cumming Tartan</title>
+<meta name="description" content="Official Website of The Clan Cumming Society of the United States">
+<meta name="keywords" content="Scottish, Scots, Clan, Cummin, Cummins, Cumming, Cummings, Comyn, Cumyn, Commings, Comings">
+<meta name="author" content="G. Allen Cummings, Rowlett, Texas, USA">
+<style type="text/css">
+div#container
+{
+   width: 1000px;
+   position: relative;
+   margin-top: 0px;
+   margin-left: auto;
+   margin-right: auto;
+   text-align: left;
+}
+body
+{
+   background-color: #5C8A97;
+   color: #1E1E1E;
+   font-family: Verdana;
+   font-size: 13px;
+   margin: 0;
+   text-align: center;
+}
+</style>
+<style type="text/css">
+a
+{
+   color: #C80000;
+   text-decoration: underline;
+}
+a:visited
+{
+   color: #C80000;
+}
+a:active
+{
+   color: #C80000;
+}
+a:hover
+{
+   color: #C80000;
+   text-decoration: underline;
+}
+</style>
+<style type="text/css">
+#Image4
+{
+   border: 0px #000000 solid;
+}
+#wb_Text10 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+   text-align: left;
+}
+#wb_Text10 div
+{
+   text-align: left;
+}
+#wb_Text9 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+   text-align: left;
+}
+#wb_Text9 div
+{
+   text-align: left;
+}
+#Image12
+{
+   border-width: 0;
+}
+#Image11
+{
+   border-width: 0;
+}
+#wb_Text8 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+   text-align: left;
+}
+#wb_Text8 div
+{
+   text-align: left;
+}
+#wb_Text7 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+   text-align: left;
+}
+#wb_Text7 div
+{
+   text-align: left;
+}
+#Image10
+{
+   border-width: 0;
+}
+#Image9
+{
+   border-width: 0;
+}
+#wb_Text6 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+   text-align: left;
+}
+#wb_Text6 div
+{
+   text-align: left;
+}
+#wb_Text5 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+   text-align: left;
+}
+#wb_Text5 div
+{
+   text-align: left;
+}
+#Image8
+{
+   border-width: 0;
+}
+#Image7
+{
+   border-width: 0;
+}
+#wb_Text4 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+   text-align: left;
+}
+#wb_Text4 div
+{
+   text-align: left;
+}
+#wb_Text2 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+   text-align: left;
+}
+#wb_Text2 div
+{
+   text-align: left;
+}
+#Image6
+{
+   border-width: 0;
+}
+#Image5
+{
+   border-width: 0;
+}
+#wb_Text3 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+   text-align: center;
+}
+#wb_Text3 div
+{
+   text-align: center;
+}
+#Image2
+{
+   border: 0px #000000 solid;
+}
+#Image1
+{
+   border: 0px #000000 solid;
+}
+#Image3
+{
+   border: 0px #000000 solid;
+}
+#wb_Text1 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+   text-align: left;
+}
+#wb_Text1 div
+{
+   text-align: left;
+}
+</style>
+</head>
+<body>
+<div id="container">
+<div id="wb_Shape3" style="position:absolute;left:210px;top:175px;width:791px;height:1236px;z-index:0;">
+<img src="images/img0046.gif" id="Shape3" alt="" style="border-width:0;width:791px;height:1236px;"></div>
+<div id="wb_Image4" style="position:absolute;left:514px;top:1339px;width:177px;height:33px;z-index:1;">
+<img src="images/thistle.png" id="Image4" alt="" style="width:177px;height:33px;"></div>
+<div id="wb_Text10" style="position:absolute;left:686px;top:1271px;width:227px;height:32px;z-index:2;text-align:left;">
+<span style="color:#1E1E1E;font-family:Verdana;font-size:13px;">Comyn Cumming Buchan (wr2012)</span></div>
+<div id="wb_Text9" style="position:absolute;left:301px;top:1273px;width:199px;height:32px;z-index:3;text-align:left;">
+<span style="color:#1E1E1E;font-family:Verdana;font-size:13px;">Comyn Cumming Clan (wr288)</span></div>
+<div id="wb_Image12" style="position:absolute;left:691px;top:1077px;width:224px;height:184px;z-index:4;">
+<img src="images/img0055.png" id="Image12" alt="" style="width:224px;height:184px;"></div>
+<div id="wb_Image11" style="position:absolute;left:288px;top:1077px;width:224px;height:184px;z-index:5;">
+<img src="images/img0054.png" id="Image11" alt="" style="width:224px;height:184px;"></div>
+<div id="wb_Text8" style="position:absolute;left:701px;top:1030px;width:202px;height:32px;z-index:6;text-align:left;">
+<span style="color:#1E1E1E;font-family:Verdana;font-size:13px;">Cumming of Glenochy (wr1902</span></div>
+<div id="wb_Text7" style="position:absolute;left:316px;top:1032px;width:166px;height:32px;z-index:7;text-align:left;">
+<span style="color:#1E1E1E;font-family:Verdana;font-size:13px;">Cumming (wr508)<br>Wilsons of Bannockburn</span></div>
+<div id="wb_Image10" style="position:absolute;left:690px;top:830px;width:224px;height:184px;z-index:8;">
+<img src="images/img0053.png" id="Image10" alt="" style="width:224px;height:184px;"></div>
+<div id="wb_Image9" style="position:absolute;left:288px;top:832px;width:224px;height:184px;z-index:9;">
+<img src="images/img0052.png" id="Image9" alt="" style="width:224px;height:184px;"></div>
+<div id="wb_Text6" style="position:absolute;left:698px;top:777px;width:204px;height:16px;z-index:10;text-align:left;">
+<span style="color:#1E1E1E;font-family:Verdana;font-size:13px;">Cumming of Glenochy (wr507)</span></div>
+<div id="wb_Text5" style="position:absolute;left:285px;top:775px;width:233px;height:32px;z-index:11;text-align:left;">
+<span style="color:#1E1E1E;font-family:Verdana;font-size:13px;">Cumming Comyn Ancient (wr1158)<br>Mill:&nbsp; House of Edgar</span></div>
+<div id="wb_Image8" style="position:absolute;left:689px;top:575px;width:224px;height:184px;z-index:12;">
+<img src="images/img0051.png" id="Image8" alt="" style="width:224px;height:184px;"></div>
+<div id="wb_Image7" style="position:absolute;left:290px;top:541px;width:224px;height:224px;z-index:13;">
+<img src="images/img0050.png" id="Image7" alt="" style="width:224px;height:224px;"></div>
+<div id="wb_Text4" style="position:absolute;left:708px;top:488px;width:183px;height:32px;z-index:14;text-align:left;">
+<span style="color:#1E1E1E;font-family:Verdana;font-size:13px;">Cumming Hunting (wr1991)<br>Mill:&nbsp; Lochcarron</span></div>
+<div id="wb_Text2" style="position:absolute;left:289px;top:488px;width:222px;height:32px;z-index:15;text-align:left;">
+<span style="color:#1E1E1E;font-family:Verdana;font-size:13px;">Cumming Red - Modern (wr1157)<br>Mill:&nbsp; Lochcarron</span></div>
+<div id="wb_Image6" style="position:absolute;left:689px;top:261px;width:224px;height:224px;z-index:16;">
+<img src="images/img0049.png" id="Image6" alt="" style="width:224px;height:224px;"></div>
+<div id="wb_Image5" style="position:absolute;left:291px;top:259px;width:224px;height:224px;z-index:17;">
+<img src="images/img0048.png" id="Image5" alt="" style="width:224px;height:224px;"></div>
+<div id="wb_Text3" style="position:absolute;left:212px;top:202px;width:783px;height:23px;text-align:center;z-index:18;">
+<span style="color:#044E1C;font-family:Georgia;font-size:19px;"><em>Cumming Tartan</em></span></div>
+<div id="wb_Shape1" style="position:absolute;left:7px;top:0px;width:995px;height:168px;z-index:19;">
+<img src="images/img0047.gif" id="Shape1" alt="" style="border-width:0;width:995px;height:168px;"></div>
+<div id="wb_Image2" style="position:absolute;left:10px;top:3px;width:162px;height:162px;z-index:20;">
+<img src="images/cumming_modern.gif" id="Image2" alt="" style="width:162px;height:162px;"></div>
+<div id="wb_Image1" style="position:absolute;left:172px;top:3px;width:160px;height:162px;z-index:21;">
+<img src="images/cumming_hunting.gif" id="Image1" alt="" style="width:160px;height:162px;"></div>
+<div id="wb_Image3" style="position:absolute;left:119px;top:7px;width:113px;height:152px;z-index:22;">
+<img src="images/tranparency_188.png" id="Image3" alt="" style="width:113px;height:152px;"></div>
+<div id="wb_Text1" style="position:absolute;left:351px;top:63px;width:644px;height:38px;z-index:23;text-align:left;">
+<span style="color:#044E1C;font-family:Georgia;font-size:32px;"><em>Clan Cumming Society of the United States</em></span></div>
+<div id="Html1" style="position:absolute;left:1px;top:0px;width:185px;height:35px;z-index:24">
+<?php include "menu.php"; ?></div>
+</div>
+</body>
+</html>
