db86b5a484567e11bbc5f8a4ac1a9c853a942183
johntrombly <john@trombly.org>
2014-06-14 15:56:34 -0500

    Update datacon2.php
    Made some Lookie Lookie comments in the code :-)

--- datacon2.php
+++ datacon2.php
@@ -1,5 +1,5 @@
 <?php
-
+// this is a comment line from John
 $dblink = mysqli_connect("localhost","root","","cumm7860_milhist");
 
 // Check connection
@@ -7,5 +7,7 @@ if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }	  
+
+// Lookie Lookie... Another comment
 
 ?>
